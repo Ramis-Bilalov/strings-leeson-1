5ddb6771f549cc9af42ea439ff13a07df61e6f45
RamisDeveloper <ramiska81@gmail.com>
2024-10-28 20:23:57 +0300

    Выполнил домашнее задание по уроку "Строки"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Задача 1.
+
+        System.out.println("Задача 1.");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+//        Задача 2.
+
+        System.out.println("\nЗадача 2.");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+//        Задача 3.
+
+        System.out.println("\nЗадача 3.");
+
+        fullName = "Иванов Семён Семёнович";
+
+        fullName = fullName.replace('ё', 'е');
+
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName);
     }
 }
